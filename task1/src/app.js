02c7cfc93f058952a801e1a190839b3ae82225d4
lordxsumit <sum56it@gmail.com>
2026-06-22 18:17:50 +0530

    Initializing internship tasks

---
+++ task1/src/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+
+
+// Import routes
+
+
+// Routes declaration
+
+
+
+export {app}
---
+++ task1/src/index.js
@@ -0,0 +1,18 @@
+import { app } from "./app.js";
+import dotenv from 'dotenv';
+import {connectDB} from './db/db.js'
+
+dotenv.config({
+    path: './.env'
+})
+
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection error: ", err);
+})
