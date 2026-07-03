d15549401ce0883c0900e500b3688d288c49ab0c
lordxsumit <sum56it@gmail.com>
2026-07-04 01:43:06 +0530

    File structure setup for the Task2

---
+++ task2/backend/src/index.js
@@ -0,0 +1,6 @@
+// TASK 2: Event Registration System 
+// ● Set up backend using Django or Express.js to manage routes and logic. 
+// ● Create models for events and user registrations in your database (like PostgreSQL, MongoDB etc.). 
+// ● Build API endpoints to view event list, event details, and submit registration forms. 
+// ● Link registrations to users and events, and allow users to manage (view/cancel) their registrations. 
+// ● Optional: Add admin panel or authentication for event organizers.
